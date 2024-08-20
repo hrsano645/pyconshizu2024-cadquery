f3a78aa0105de6dae0c5f3c62fa372addd44cc9b
hrsano645 <hrs.sano645@gmail.com>
2024-08-20 11:20:00 +0900

    wip update

--- ex1_hako.py
+++ ex1_hako.py
@@ -1,2 +1,23 @@
 import cadquery as cq
 from ocp_vscode import show_object, show
+
+# 単位は全部mm
+
+# 四角形の箱、サイズは 10x10x10
+box = cq.Workplane("XY").box(10, 10, 10)
+
+
+# 次に 内側をくりぬいていく。壁の厚みは 2mm
+## まずは、外側の壁を作る
+box = cq.Workplane("XY").box(10, 10, 10)
+
+
+# 多角形もできる。（5角形で例
+
+# 円筒も
+
+# とりあえず箱的な物はこうやってできるよ
+
+ふたを作ってみよう
+
+#
