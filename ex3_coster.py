287dc633500acc6afbb2880f82280f5fd46f99a2
Hiroshi Sano <hrs.sano645@gmail.com>
2024-09-20 17:12:23 +0900

    wip update

--- ex3_coster.py
+++ ex3_coster.py
@@ -3,7 +3,7 @@
 import cadquery as cq
 from ocp_vscode import show_object
 
-# コースターの半径 * 厚み 50mm x 6mm
+# コースターの半径 * 厚み 50mm x 3mm
 coaster_size = 50
 coaster_tickness = 3
 
@@ -14,9 +14,10 @@ logo_size = 56
 
 # ファイルを読み込む
 python_logo_rinkaku = cq.importers.importDXF("asset/python-logo-only_rinkaku.dxf")
-# show_object(python_logo_rinkaku, name="Python Logo", )
+# show_object(python_logo_rinkaku, name="Python Logo")
 
 # 輪郭のみを取り出して押し出し、中心位置に持ってくる
+# DXFからワイヤーのみ(wires)を取り出す（toPending）、押し出す（extrude）
 logo_obj = (
     cq.Workplane("XY")
     .add(python_logo_rinkaku)
@@ -25,7 +26,10 @@ logo_obj = (
     .extrude(3)
     .translate((0, 0, 2))
 )
-# show_object(logo_obj, name="Python Logo model", )
+show_object(
+    logo_obj,
+    name="Python Logo model",
+)
 
 # コースターの土台を作る。円筒を作り下面の外周にフィレットをかける
 coaster_base = (
@@ -41,7 +45,10 @@ coaster_base = (
     .faces(">X")
     .fillet(0.5)
 )
-# show_object(coaster_base, name="Coaster base", )
+show_object(
+    coaster_base,
+    name="Coaster base",
+)
 
 # 輪郭をコースターに切り抜く
 coaster_result = coaster_base.cut(logo_obj)
@@ -54,4 +61,3 @@ show_object(
 
 # STLで書き出す: トレランスを細かくして輪郭を綺麗にする
 cq.exporters.export(coaster_result, "exports/ex3_coster.stl", tolerance=0.001)
-cq.exporters.export(coaster_result, "exports/ex3_coster.step", tolerance=0.001)
